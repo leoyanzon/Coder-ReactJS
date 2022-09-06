07de5526731a72f1ec3527fa09d07ae046e07275
leoyanzon <leoyanzon@gmail.com>
2022-09-06 19:09:27 -0300

    2da subida Consumiendo APIs

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import "./App.css";
-import React from "react";
 import NavBar from "./Components/Header/NavBar";
 import { ItemListContainer } from "./Components/Items/ItemListContainer";
-import {ItemDetailContainer} from './Components/Items/ItemDetailContainer'
-import {useState, useEffect} from 'react'
-
-// DATA
-
-const baseDeDatos = [
-  {
-      id: 1,
-      title: "Robot",
-      price: 10,
-      pictureUrl: "https://images.cults3d.com/st8f6YX2-lyxUfTjqDaNgdbr_8o=/516x516/filters:format(webp)/https://files.cults3d.com/uploaders/14581776/illustration-file/216ff4b1-cb11-4352-a2df-7d9b7c4b82ad/Rendered-3.png"
-  },
-  {
-      id: 2,
-      title: "Transformer",
-      price: 20,
-      pictureUrl: "https://images.cults3d.com/5Xm2bQksB5uwtv0Quk3hDjW7Ilk=/516x516/filters:format(webp)/https://files.cults3d.com/uploaders/23832575/illustration-file/67d4fae3-4e56-485e-90da-f2a35e2e18ae/basePrint.jpg"
-  },
-  {
-      id: 3,
-      title: "Playmobil",
-      price: 5,
-      pictureUrl: "https://images.cults3d.com/R4sqJeSZ4zVe-cZTfhX4dqhemuE=/516x516/https://files.cults3d.com/uploaders/14187328/illustration-file/9d4ec608-a95f-4bd4-a3ee-b6cff2b56c1e/7ed15a90f746707e13a1b18817d9bc2c_display_large.jpg#1"
-  }
-];
+import { ItemDetailContainer } from './Components/Items/ItemDetailContainer'
 
 
 const App = () => {
-  const [items, setItems] = useState([])
 
-
-  //Promesa ficticia simulando retardo de api
-      const obtenerDatosApi = () => {
-          return new Promise((resolve, reject) => {
-              setTimeout(()=>{
-                  resolve(baseDeDatos)
-              }, 2000)
-          })
-      }
-  
-  //Use efect se ejecuta UNA VEZ que fue renderizado el componente. Cuando termina de renderizarse
-  
-      useEffect(()=>{ // Si tiene [] se ejecuta 1 sola vez solamente despues del primer renderizado.
-          const funcionAsincronaObtener = async() => {
-              try{
-                  const listado = await obtenerDatosApi();
-                  setItems (listado);
-              }
-              catch{
-                  console.log("Ocurrió un error");
-              }
-          }
-          funcionAsincronaObtener();
-      },[]);
 
   return(
     <>
       <NavBar></NavBar>
-      <ItemListContainer items ={items}/>
-      <ItemDetailContainer prodId = {items}/>
+      <ItemListContainer/>
+      <ItemDetailContainer/>
     </>
   )
 
--- src/Components/Items/ItemDetailContainer.js
+++ src/Components/Items/ItemDetailContainer.js
@@ -1,9 +1,58 @@
 import { ItemDetail } from './ItemDetail';
 import { useEffect, useState } from 'react';
 
-export const ItemDetailContainer = ({prodId}) => {
-    
+export const ItemDetailContainer = () => {
+    const [items, setItems] = useState([])
     const [producto, setProducto] = useState(0);
+    
+    // DATA
+
+    const baseDeDatos = [
+        {
+            id: 1,
+            title: "Robot",
+            price: 10,
+            pictureUrl: "https://images.cults3d.com/st8f6YX2-lyxUfTjqDaNgdbr_8o=/516x516/filters:format(webp)/https://files.cults3d.com/uploaders/14581776/illustration-file/216ff4b1-cb11-4352-a2df-7d9b7c4b82ad/Rendered-3.png"
+        },
+        {
+            id: 2,
+            title: "Transformer",
+            price: 20,
+            pictureUrl: "https://images.cults3d.com/5Xm2bQksB5uwtv0Quk3hDjW7Ilk=/516x516/filters:format(webp)/https://files.cults3d.com/uploaders/23832575/illustration-file/67d4fae3-4e56-485e-90da-f2a35e2e18ae/basePrint.jpg"
+        },
+        {
+            id: 3,
+            title: "Playmobil",
+            price: 5,
+            pictureUrl: "https://images.cults3d.com/R4sqJeSZ4zVe-cZTfhX4dqhemuE=/516x516/https://files.cults3d.com/uploaders/14187328/illustration-file/9d4ec608-a95f-4bd4-a3ee-b6cff2b56c1e/7ed15a90f746707e13a1b18817d9bc2c_display_large.jpg#1"
+        }
+    ];
+
+  //Promesa ficticia simulando retardo de api
+    const obtenerDatosApi = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                resolve(baseDeDatos)
+            }, 2000)
+        })
+    }
+
+    //Use efect se ejecuta UNA VEZ que fue renderizado el componente. Cuando termina de renderizarse
+    
+    useEffect(()=>{ // Si tiene [] se ejecuta 1 sola vez solamente despues del primer renderizado.
+        const funcionAsincronaObtener = async() => {
+            try{
+                const listado = await obtenerDatosApi();
+                setItems (listado);
+            }
+            catch{
+                console.log("Ocurrió un error");
+            }
+        }
+        funcionAsincronaObtener();
+    },[]);
+
+
 
     useEffect(()=>{
         const seleccionProducto = () => {
@@ -12,19 +61,19 @@ export const ItemDetailContainer = ({prodId}) => {
                 }, 2000);
         }
         seleccionProducto();         
-    },[prodId]);
+    },[items]);
 
     const cambiarProducto = () => {
-         if (prodId.length > 0){
-            const seleccion = prodId.find(it => it.id === 2);
+         if (items.length > 0){
+            const seleccion = items.find(it => it.id === 2);
             setProducto(seleccion.id);
             }
     }
 
     return(
         <>
-            {   prodId.length > 0 &&
-            <ItemDetail producto = {prodId[producto]}></ItemDetail>
+            {   items.length > 0 &&
+            <ItemDetail producto = {items[producto]}></ItemDetail>
             }
         </>
     )
